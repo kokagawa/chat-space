1e0a254a1a25f65ded1e90274a93210f3cc10403
kagawa <tekken77marduk>
2019-08-27 20:03:18 +0900

    インクリメンタルサーチ

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,19 +4,22 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @group.users << current_user
-    
+ 
   end
 
   def create
+   
     @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
+    
   end
 
   def edit
+  
   end 
 
   def group_params
@@ -26,11 +29,13 @@ class GroupsController < ApplicationController
 
 
   def update
+    
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
+ 
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: 
+      current_user.id).limit(20)
+      respond_to do |format|
+        format.html 
+        format.json 
+        
+       
+      end
+  end
+
   def edit
   end
 
   def update
+   
     if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
     end
+   
   end
 
   private
