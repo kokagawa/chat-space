7392606c7e7f623d3fc6dfbce2bc61dadf600575
kagawa <tekken77marduk>
2019-08-31 12:04:31 +0900

    upup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
     resources :messages, only: [:index, :create]
-  
+    namespace :api do #APIのコントローラを動かすための記述
+      resources :messages, only: :index, defaults: { format: 'json' } #defaultsオプションを利用して、このルーティングが来たらjson形式でレスポンスするよう指定。
+    end
 end
 end
