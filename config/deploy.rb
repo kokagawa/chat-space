a614f25c50fdd10f8ef6740e5b6493a37ba23f38
kagawa <tekken77marduk>
2019-09-06 11:27:51 +0900

    s

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/tekkennina.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/tekkennina.pem']  #例：~/.ssh/key_pem.pem
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
